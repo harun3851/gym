cc41904711d9f76c6ff545222d4b9f997fba85d6
Harun Koyuncu <30460243+harun3851@users.noreply.github.com>
2023-07-22 20:18:28 +0300

    navbar transparancy with scrolling

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,31 @@
 
 import Navbar from "@/scenes/navbar";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { SelectedPage } from "./shared/types";
 
+type Props ={
+  selectedPage:SelectedPage;
+  setSelectedPage:(value:SelectedPage)=>void;
+}
 
 
 
 function App() {
   const [selectedPage, setSelectedPage] = useState<SelectedPage>(SelectedPage.Home);
   const [isTopOfPage, setIsTopOfPage] = useState<boolean>(true);
+
+  useEffect(()=>{
+    const handleScroll = ()=>{
+      if(window.scrollY===0){
+        setIsTopOfPage(true);
+        setSelectedPage(SelectedPage.Home);
+      }
+      if(window.screenY !==0) setIsTopOfPage(false);
+    }
+    window.addEventListener("scroll",handleScroll);
+    return () =>window.removeEventListener("scroll", handleScroll);
+
+  },[]);
 
   return (
     
